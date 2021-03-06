aa5b1ee1e6ba4ba6598f3ee98197a0ce5a083d3a
smpny7 <symphonymode7@icloud.com>
2021-03-06 10:40:31 +0900

    電算研公式キャラに変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,7 @@
 module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
-    theme: {
-        fontFamily: {
-            'default': ['Rock Salt'],
-        },
-        extend: {
-            inset: {
-                '1/5': '20%',
-                '19/50': '38%',
-            }
-        },
-    },
+    theme: {},
     variants: {
         extend: {},
     },
